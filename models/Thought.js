3af350a7a55bff27d7473d5e44effc2780d5c015
Saritha Koduru <saritha.2708@gmail.com>
2023-06-13 20:59:57 -0700

    added data models

---
+++ models/Thought.js
@@ -0,0 +1,77 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils');
+
+const reactionSchema = new Schema(  
+    {
+        // set custom id to avoid confusion with parent comment's _id field
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // use getter method to format timestamp
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // use getter method to format timestamp
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true,
+            ref: 'User'
+        },
+        // use ReactionSchema to validate data for a reply
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false
+    }
+);
+
+// get total count of reactions on retrieval
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+}
+);
+
+// create the Thought model using the ThoughtSchema
+const Thought = model('Thought', thoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
